052af8edcd2a0a853ff32bb9ba9dfc6013f6c2aa
jysn07 <jysnbrgs07@gmail.com>
2026-07-29 20:59:53 +0800

    Remove missing JS references

--- resources/views/homepageSection/contactUsForm.blade.php
+++ resources/views/homepageSection/contactUsForm.blade.php
@@ -105,8 +105,3 @@
 		</div>
 	</section>
 
-	<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.validate.min.js"></script>
-  <script src="js/main.js"></script>
